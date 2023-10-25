80ee3ad2b68daf998cb712dec67760c6ee41e656
Sergei Bozhko <sbozhko@mpi-sws.org>
2023-10-25 22:08:21 +0200

    prove lemma: [F ⊑ H -> G ⊑ H -> (F ⊕ G) ⊑ O(H)]

--- ONotation.v
+++ ONotation.v
@@ -152,6 +152,88 @@ Section Main.
 
       (* ------------------------------- Plus ------------------------------- *)
 
+      Lemma comp_incl_max:
+        forall (F : ParamCompl X) (f g : X -> nat),
+          f ∈p F -> g ∈p F ->
+          (fun x => max (f x) (g x)) ∈p F.
+      Proof.
+        induction F as [F | | | | | | ]; intros * IN1 IN2.
+        - now intros ?; apply Max.max_lub.
+        - destruct IN1 as [fo [IN3 LE1]], IN2 as [go [IN4 LE2]].
+          specialize (IHF _ _ IN3 IN4).
+          exists (fun x => max (fo x) (go x)); split.
+          + exact IHF.
+          + intros a b.
+            specialize (LE1 a b); destruct LE1 as [δ1 LE3].
+            specialize (LE2 a b); destruct LE2 as [δ2 LE4].
+            exists (max δ1 δ2); intros.
+            destruct (Max.max_dec (f x) (g x)) as [EQ|EQ].
+            rewrite EQ; clear EQ; rewrite LE3; lia.
+            rewrite EQ; clear EQ; rewrite LE4; lia.
+        - destruct IN1 as [fo [IN3 LE1]], IN2 as [go [IN4 LE2]].
+          specialize (IHF _ _ IN3 IN4).
+          exists (fun x => max (fo x) (go x)); split.
+          + exact IHF.
+          + destruct LE1 as (a1 & b1 & LE1), LE2 as (a2 & b2 & LE2).
+            exists (max a1 a2), (max b1 b2); intros.
+            specialize (LE1 x); specialize (LE2 x).
+            destruct (Max.max_dec (f x) (g x)) as [EQ|EQ].
+            now rewrite EQ; clear EQ; rewrite LE1; nia.
+            now rewrite EQ; clear EQ; rewrite LE2; nia.
+        - destruct IN1 as [f1 [f2 [IN3 [IN4 LE1]]]], IN2 as [g1 [g2 [IN5 [IN6 LE2]]]].
+          specialize (IHF1 _ _ IN3 IN5); specialize (IHF2 _ _ IN4 IN6).
+          exists (fun x => max (f1 x) (g1 x)), (fun x => max (f2 x) (g2 x)); repeat split; try easy.
+          now intros; specialize (LE1 x); specialize (LE2 x); lia.
+        - destruct IN1 as [f1 [f2 [IN3 [IN4 LE1]]]], IN2 as [g1 [g2 [IN5 [IN6 LE2]]]].
+          specialize (IHF1 _ _ IN3 IN5); specialize (IHF2 _ _ IN4 IN6).
+          exists (fun x => max (f1 x) (g1 x)), (fun x => max (f2 x) (g2 x)); repeat split; try easy.
+          now intros; specialize (LE1 x); specialize (LE2 x); nia.
+        - destruct IN1 as (f1 & f2 & POS1 & IN3 & IN4 & LE1);
+            destruct IN2 as (g1 & g2 & POS2 & IN5 & IN6 & LE2).
+          specialize (IHF1 _ _ IN3 IN5); specialize (IHF2 _ _ IN4 IN6).
+          exists (fun x => max (f1 x) (g1 x)), (fun x => max (f2 x) (g2 x)); repeat split; try easy.
+          + now intros; specialize (POS1 x); specialize (POS2 x); nia.
+          + intros; apply Max.max_lub.
+            * rewrite LE1; apply PeanoNat.Nat.pow_le_mono.
+              -- now apply PeanoNat.Nat.neq_0_lt_0, POS1.
+              -- now apply PeanoNat.Nat.le_max_l.
+              -- now apply PeanoNat.Nat.le_max_l.
+            * rewrite LE2; apply PeanoNat.Nat.pow_le_mono.
+              -- now apply PeanoNat.Nat.neq_0_lt_0, POS2.
+              -- now apply PeanoNat.Nat.le_max_r.
+              -- now apply PeanoNat.Nat.le_max_r.
+        - destruct IN1 as (F1 & MON1 & f2 & IN3 & LE1);
+            destruct IN2 as (G1 & MON2 & g2 & IN5 & LE2).
+          specialize (IHF _ _ IN3 IN5).
+          exists (fun x => max (F1 x) (G1 x)); split.
+          + intros ? ? LE.
+            apply Max.max_lub.
+            * now apply PeanoNat.Nat.max_le_iff; left; apply MON1.
+            * now apply PeanoNat.Nat.max_le_iff; right; apply MON2.
+          + exists (fun x : X => max (f2 x) (g2 x)); repeat split; try easy.
+            intros; destruct (Max.max_dec (f2 x) (g2 x)) as [EQ|EQ]; rewrite EQ.
+            * apply Max.max_lub.
+              -- now rewrite LE1; apply PeanoNat.Nat.max_le_iff; left; easy.
+              -- rewrite LE2; apply PeanoNat.Nat.max_le_iff; right.
+                 now apply MON2; rewrite <-EQ; apply PeanoNat.Nat.le_max_r.
+            * apply Max.max_lub.
+              -- rewrite LE1; apply PeanoNat.Nat.max_le_iff; left.
+                 now apply MON1; rewrite <-EQ; apply PeanoNat.Nat.le_max_l.
+              -- now rewrite LE2; apply PeanoNat.Nat.max_le_iff; right; easy.
+      Qed.
+
+      Lemma comp_incl_plus:
+         forall (F : ParamCompl X) (f g : X -> nat),
+           f ∈p F -> g ∈p F ->
+           (fun x => f x + g x) ∈p O (F).
+      Proof.
+        intros * IN1 IN2.
+        exists (fun x => max (f x) (g x)); repeat split.
+        - now apply comp_incl_max.
+        - exists 2, 0; intros.
+          now destruct (Max.max_dec (f x) (g x)) as [EQ|EQ]; lia.
+      Qed.
+
       Lemma comp_incl_plus_compat:
         forall (F1 F2 G1 G2 : ParamCompl X),
           F1 ⊑ F2 -> G1 ⊑ G2 -> F1 ⊕ G1 ⊑ F2 ⊕ G2.
@@ -170,6 +252,19 @@ Section Main.
         forall (F1 F2 G : ParamCompl X),
           F1 ⊑ F2 -> F1 ⊕ G ⊑ F2 ⊕ G.
       Proof. now intros * IN; apply comp_incl_plus_compat. Qed.
+
+      Lemma comp_incl_app :
+        forall (F G H : ParamCompl X),
+          F ⊑ H -> G ⊑ H -> (F ⊕ G) ⊑ O(H).
+      Proof.
+        intros * SUB1 SUB2 ? IN.
+        destruct IN as [f1 [f2 [IN1 [IN2 LE]]]].
+        specialize (SUB1 _ IN1); specialize (SUB2 _ IN2).
+        apply coml_incl_O_idem.
+        exists (fun x => f1 x + f2 x); split.
+        - now apply comp_incl_plus.
+        - now exists 1, 0; intros; specialize (LE x); nia. 
+      Qed.
 
       (* ------------------------------- Mult ------------------------------- *)
 
